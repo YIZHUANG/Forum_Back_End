f61181779ab228f078ac475b1b26c51116bbde24
YI ZHUANG <urlikeahurricane@gmail.com>
2017-12-10 16:40:14 +0200

    add timestamp to comment

--- model/post.js
+++ model/post.js
@@ -6,12 +6,13 @@ const threadSchema = new Schema({
   title: String,
   author:String,    //the person who make the post
   content:String,
-  imageURL:String,  
+  imageURL:String,
   date: {type: Date,default: Date.now },  //the creation date of the post
   votes:{type:Number,default:0},  //will be added later on.
   comment:[{     //default null.
     postMan:String,   //Name of the person who make the comment
-    commentBody:String  //the content of the comment
+    commentBody:String,  //the content of the comment
+    date: {type: Date,default: Date.now }
   }]
 });
 
