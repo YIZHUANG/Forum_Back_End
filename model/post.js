5e646e9d83a355762807cb4de2e6e35141bc988c
YI ZHUANG <urlikeahurricane@gmail.com>
2017-12-10 14:23:15 +0200

    add comment

--- model/post.js
+++ model/post.js
@@ -4,11 +4,15 @@ const {Schema} = mongoose;
 
 const threadSchema = new Schema({
   title: String,
-  author:String,
+  author:String,    //the person who make the post
   content:String,
   imageURL:String,
-  date: {type: Date,default: Date.now },
-  votes:{type:Number,default:0}
+  date: {type: Date,default: Date.now },  //the creation date of the post
+  votes:{type:Number,default:0},  //will be added later on.
+  comment:[{     //default null.
+    postMan:String,   //Name of the person who make the comment
+    commentBody:String  //the content of the comment
+  }]
 });
 
 mongoose.model('threads', threadSchema);
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-require('./model/post');
-const Threads=mongoose.model('threads');
-const url = "mongodb://YI:a1234@ds135916.mlab.com:35916/forum";
-mongoose.connect(url);
+require('./model/post'); //import the schema
+const Threads=mongoose.model('threads'); //import the schema
+const url = "mongodb://YI:a1234@ds135916.mlab.com:35916/forum";  // the url of the databse.
+mongoose.connect(url); //connent!
 var app = express();
-var cors = require('cors');
-app.use(cors());
+var cors = require('cors'); // allow corss-domain communication.
+app.use(cors());  // allow corss-domain communication.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
  extended: false
 }));
 
-app.get('/listOfPosts', function(request, response) {
+app.get('/listOfPosts', function(request, response) {     // all the posts are saved to this route
  Threads.find({}).then(eachOne => {
     response.json(eachOne);
-    })
-  })
+  });
+});
 
-app.post('/api',function(req,res){
+app.post('/api',function(req,res){    //post new threads. will be saved stright to the database and rendern in the front-end side.
   var threads=new Threads();
   threads.title=req.body.title;
   threads.author=req.body.author;
@@ -32,8 +32,32 @@ app.post('/api',function(req,res){
     else{
       res.send(savedPosts);
     }
-  })
-})
+  });
+});
+
+app.put('/comment/:postId',function(req,res){   //save comments to the post
+  var threads=new Threads();
+  var comments=req.body;
+  if(!comments||comments===""){             //error handling, comment must have text.
+    res.status(500).send({error:"Your comment mush have some text"});
+  }else{
+    Threads.findOne({_id:req.params.postId},function(error,posts){    // search for the matching postId in the database, it will show errors if no match.
+      if(error){
+        res.status(500).send({error:'could not find post'});
+      }else{                                                                 //standard mongoDB update queries.
+        Threads.update({_id:req.params.postId},{$addToSet:{comment:comments}},function(error,posts){  //if match found, then save the comments to the specific post
+          if(error){
+            res.status(500).send({error:"something went wrong,perhaps the post dose not exsit"});
+          }else{
+            res.send("success commentted");
+          }
+        });
+      }
+    });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT,function(){
+  console.log("Api is running at port " + PORT)
+});
