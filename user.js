ce9bad9e3492df8bcb893fae0dabb5324dc8cfc1
YI ZHUANG <urlikeahurricane@gmail.com>
2017-12-19 21:48:02 +0200

    ade

--- user.js
+++ user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const assert=require('assert')
+
 
 require("./model/user"); //import the schema
 const User = mongoose.model("user"); //import the schema
@@ -10,14 +10,5 @@ module.exports={
           "user_name":user_name,
           "password":password
         })
-    },
-  validateSignIn:(user_name,password,callback)=>{
-      User.findOne({user_name:user_name,password:password},(error,result)=>{
-        if(!result){
-          callback(false)
-        }else{
-          callback(true)
-        }
-      })
-  }
+    }
 }
